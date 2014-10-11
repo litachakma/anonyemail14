e63e0fef8065d7234b2f48579af784c16429870f
litachakma <chakma.lita@gmail.com>
2014-10-11 12:08:51 -0700

    latest

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
     
 import os
 import urllib
+import logging
+import cgi
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
+
+from google.appengine.api import mail
 
 import jinja2
 import webapp2
@@ -40,9 +45,53 @@ class TermsPage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 
+class SendEmail(webapp2.RequestHandler):
+    def post(self):
+        
+        # cgi.escape(self.request.get('content'))
+
+        to = cgi.escape(self.request.get('to'))
+        message = mail.EmailMessage(sender=cgi.escape(self.request.get('from')),
+                            subject=cgi.escape(self.request.get('subject')))
+
+        message.to = to
+        message.body = cgi.escape(self.request.get('message'))
+
+        message.send()
+        self.response.write("Sent email to: " + to)        
+
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
      ('/terms', TermsPage),
+     ('/sendmail', SendEmail),
 ], debug=True)
 
 
+class LogSenderHandler(InboundMailHandler):
+    def receive(self, mail_message):
+        logging.info("================================")
+        logging.info("Received a mail_message from: " + mail_message.sender)
+        logging.info("The email subject: " + mail_message.subject)
+       
+        try:
+            logging.info("The email was addressed to: " + str.join(mail_message.to, ', '))
+            logging.info("The email was CC-ed to: " + str.join(mail_message.cc, ', '))
+        except exceptions.AttributeError :
+            logging.info("The email has no CC-ed recipients")
+
+        try:
+            logging.info("The email was send on: " + str(mail_message.date))
+        except exceptions.AttributeError :
+            logging.info("The email has no send date specified!!!")
+
+        plaintext_bodies = mail_message.bodies('text/plain')
+        html_bodies = mail_message.bodies('text/html')
+
+        for content_type, body in html_bodies:
+            decoded_html = body.decode()
+            logging.info("content_type: " + content_type)
+            logging.info("decoded_html: " + decoded_html)
+
+mailer = webapp2.WSGIApplication([LogSenderHandler.mapping()], debug=True)
